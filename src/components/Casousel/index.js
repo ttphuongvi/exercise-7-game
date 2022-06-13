36bc38981ccbbde620c5dd0c5c0c3490dbb12a98
ttphuongvi <trthphuongvi2901@gmail.com>
2022-06-13 16:37:43 +0700

    carosel game in home none set image

---
+++ src/components/Casousel/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Carousel } from "react-carousel-minimal";
+import data from "./data";
+const captionStyle = {
+  fontSize: "2em",
+  fontWeight: "bold",
+};
+const slideNumberStyle = {
+  fontSize: "20px",
+  fontWeight: "bold",
+};
+const Casousel = () => {
+  return (
+    <Carousel
+      data={data}
+      time={2000}
+      width="100%"
+      height="500px"
+      captionStyle={captionStyle}
+      //   radius="10px"
+      //   slideNumber={true}
+      slideNumberStyle={slideNumberStyle}
+      captionPosition="bottom"
+      automatic={true}
+      dots={true}
+      pauseIconColor="white"
+      pauseIconSize="40px"
+      slideBackgroundColor="darkgrey"
+      //   slideImageFit="cover"
+      thumbnails={true}
+      thumbnailWidth="100px"
+      style={{
+        textAlign: "center",
+        maxWidth: "100%",
+        maxHeight: "500px",
+        margin: "40px auto",
+      }}
+    />
+  );
+};
+
+export default Casousel;
