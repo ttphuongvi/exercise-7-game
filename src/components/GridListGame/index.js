24157ce4af99291432969d5e81b039809fe9fb49
ttphuongvi <trthphuongvi2901@gmail.com>
2022-06-15 17:22:00 +0700

    set release year for page ListGame

---
+++ src/components/GridListGame/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GridListGame = () => {
+  return <div>GridListGame;</div>;
+};
+
+export default GridListGame;
--- src/screens/ListGame/index.js
+++ src/screens/ListGame/index.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-
-import Button from "./../../components/Button";
+import React from "react";
 import Dialog from "../../components/Dialog";
 import Title from "../../components/Title";
 import "./styles.css";
+import dataYear from "./dataYear.js";
 const ListGame = () => {
-  const [option, setOption] = useState(2010);
-
   return (
     <div className="div__ListGame">
       <Title title="DANH SÁCH GAME"></Title>
       <section className="sec-1">
         <input></input>
         <select>
-          <option></option>
+          {dataYear.map((value, key) => (
+            <option key={key} value={value.value}>
+              {value.year}
+            </option>
+          ))}
         </select>
         <Dialog />
       </section>
