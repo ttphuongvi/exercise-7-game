19375edb51567fb9eee95c3579ad8fcc0b9ac3c9
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-01 15:48:13 +0700

    edit atoms box

--- src/Atomic/atoms/AtomBox.js
+++ src/Atomic/atoms/AtomBox.js
@@ -1,11 +1,4 @@
-import React from "react";
 import Box from "@material-ui/core/Box";
-const AtomBox = (props) => {
-  return (
-    <Box p={props.p} sx={props.sx}>
-      {props.children}
-    </Box>
-  );
-};
 
+const AtomBox = Box;
 export default AtomBox;
