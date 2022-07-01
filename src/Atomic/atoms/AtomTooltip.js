3a76e52a30cd57ff79fa636c86b7d11353e6a37e
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-01 15:51:02 +0700

    edit atoms tooltip

--- src/Atomic/atoms/AtomTooltip.js
+++ src/Atomic/atoms/AtomTooltip.js
@@ -1,7 +1,4 @@
-import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
-const AtomTooltip = (props) => {
-  return <Tooltip title={props.title}>{props.children}</Tooltip>;
-};
 
+const AtomTooltip = Tooltip;
 export default AtomTooltip;
