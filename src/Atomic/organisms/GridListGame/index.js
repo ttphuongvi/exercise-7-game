24a0518bb11404873d9ef8f76ca0fd357a89b7a2
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-02 16:32:53 +0700

    use DialogCreateGame

--- src/Atomic/organisms/GridListGame/index.js
+++ src/Atomic/organisms/GridListGame/index.js
@@ -2,22 +2,28 @@ import { React, useEffect, useState } from "react";
 import axios from "axios";
 import "./styles.css";
 import { Link } from "react-router-dom";
-import PlayGame from "../PlayGame";
 import ButtonStyle2 from "../../molecules/ButtonStyle2";
+import DialogPlayGame from "../../molecules/DialogPlayGame";
+import { useSelector, useDispatch } from "react-redux";
+import { SET_LIST_GAME } from "../../../redux/const";
+
 const GridListGame = (props) => {
   const [hiddenLoadding, setHidden] = useState(false);
   const onClickLoadding = () => {
     axios.get("/games?_sort=id&_order=desc").then((res) => {
-      setDataSource(res.data);
+      dispatch({ type: SET_LIST_GAME, listGame: res.data });
     });
     setHidden(true);
   };
-  const [dataSource, setDataSource] = useState([]);
+
+  const dataSource = useSelector((state) => state.listGame.content);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     axios.get("/games?_sort=id&_order=desc&_start=0&_limit=8").then((res) => {
-      setDataSource(res.data);
+      dispatch({ type: SET_LIST_GAME, content: res.data });
+      // set list tgame
     });
-    console.log(props.input);
   }, []);
   // let { path, url } = useMatch();
   return (
@@ -25,28 +31,36 @@ const GridListGame = (props) => {
       <div className="grid__list-game--row">
         <section>
           <ul class="grid__listgame">
-            {dataSource.map((value, key) => {
-              return (
-                <li key={key} class="li__grid-listgame-item">
-                  <div className="gridgame-listgame__image">
-                    <img className="img--style" src={value.image} alt=""></img>
-                  </div>
-                  <div className="grid-listgame__content">
-                    <Link to={`/${value.id}`}>
-                      {" "}
-                      <h2>{value.caption}</h2>
-                    </Link>
-                    <div className="div__release-year">
-                      Phát hành ngày {value.release}
+            {dataSource &&
+              dataSource.map((value, key) => {
+                return (
+                  <li key={key} class="li__grid-listgame-item">
+                    <div className="gridgame-listgame__image">
+                      <img
+                        className="img--style"
+                        src={value.image}
+                        alt=""
+                      ></img>
                     </div>
-                    <p className="description--justify description--justify--line-clamp-9">
-                      {value.description}
-                    </p>
-                    <PlayGame caption={value.caption} link={value.link} />
-                  </div>
-                </li>
-              );
-            })}
+                    <div className="grid-listgame__content">
+                      <Link to={`/${value.id}`}>
+                        {" "}
+                        <h2>{value.caption}</h2>
+                      </Link>
+                      <div className="div__release-year">
+                        Phát hành ngày {value.release}
+                      </div>
+                      <p className="description--justify description--justify--line-clamp-9">
+                        {value.description}
+                      </p>
+                      <DialogPlayGame
+                        caption={value.caption}
+                        link={value.link}
+                      />
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
         </section>
       </div>
