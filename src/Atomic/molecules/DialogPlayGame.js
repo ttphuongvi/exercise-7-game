f520fe43bb7fbdf3f80bc139578e8d9f0c96a58e
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-02 16:27:51 +0700

    update DialogPlayGame

--- src/Atomic/molecules/DialogPlayGame.js
+++ src/Atomic/molecules/DialogPlayGame.js
@@ -8,6 +8,7 @@ import AtomToolBar from "../atoms/AtomToolbar";
 import AtomTypography from "../atoms/AtomTypography";
 import { makeStyles } from "@material-ui/core/styles";
 import Iframe from "react-iframe";
+import ButtonStyle1 from "./ButtonStyle1";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -17,36 +18,55 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     flex: 1,
   },
+  floatRight: {
+    float: "right",
+  },
 }));
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <AtomSlide direction="up" ref={ref} {...props} />;
 });
 const DialogPlayGame = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <AtomDialog
-      fullScreen
-      open={props.open}
-      onClose={props.onClose}
-      TransitionComponent={Transition}
-    >
-      <AtomAppBar className={classes.appBar}>
-        <AtomToolBar>
-          <AtomIconButton
-            edge="start"
-            color="inherit"
-            onClick={props.onClick}
-            aria-label="close"
-          >
-            <AtomCloseIcon />
-          </AtomIconButton>
-          <AtomTypography variant="h6" className={classes.title}>
-            {props.caption}
-          </AtomTypography>
-        </AtomToolBar>
-      </AtomAppBar>
-      <Iframe url={props.link} width="100%" height="100%" />
-    </AtomDialog>
+    <div className={classes.floatRight}>
+      <ButtonStyle1 label="Chơi game" onClick={handleClickOpen}></ButtonStyle1>
+      <AtomDialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AtomAppBar className={classes.appBar}>
+          <AtomToolBar>
+            <AtomIconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <AtomCloseIcon />
+            </AtomIconButton>
+            <AtomTypography
+              component={"span"}
+              variant="h6"
+              className={classes.title}
+            >
+              {props.caption}
+            </AtomTypography>
+          </AtomToolBar>
+        </AtomAppBar>
+        <Iframe url={props.link} width="100%" height="100%" />
+      </AtomDialog>
+    </div>
   );
 };
 
