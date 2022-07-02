74902839760536fd2501e115c6abfcca8af7fbe9
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-02 16:34:39 +0700

    edit sẻach for year to DatePicker of material ui v4

--- src/Atomic/pages/ListGame/index.js
+++ src/Atomic/pages/ListGame/index.js
@@ -1,11 +1,13 @@
 import { React, useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import DialogCreateGame from "../../organisms/DialogCreateGame/index";
+import DateFnsUtils from "@date-io/date-fns";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+import DialogCreateGame from "../../molecules/DialogCreateGame";
 import Title from "../../../components/Title";
 import "./styles.css";
-import dataYear from "./dataYear.js";
 import GridListGame from "../../organisms/GridListGame";
 import "./styles.css";
+import AtomTextField from "../../atoms/AtomTextField";
 const ListGame = () => {
   const [inputText, setInputText] = useState("");
   let inputHandler = (e) => {
@@ -13,30 +15,33 @@ const ListGame = () => {
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
+  // const [currency, setCurrency] = React.useState();
+
+  // const handleChange = (event) => {
+  //   setCurrency(event.target.value);
+  // };
+  const [selectedDate, handleDateChange] = useState(new Date());
   return (
     <Title class="div__container--flex mr-t--100" title="DANH SÁCH GAME">
       <div className="div__ListGame">
         <section className="sec-1 flex--row flex--spacebetween">
           <div className="search flex--row search__input">
-            <TextField
+            <AtomTextField
               id="outlined-basic"
               onChange={inputHandler}
               variant="outlined"
               fullWidth
               label="Tìm kiếm game"
-              className="search__input"
             />
-            <select className="section__year">
-              {dataYear.map((value, key) => (
-                <option
-                  className="option__year--margin"
-                  key={key}
-                  value={value.value}
-                >
-                  {value.year}
-                </option>
-              ))}
-            </select>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DatePicker
+                views={["year"]}
+                inputVariant="outlined"
+                label="Năm phát hành"
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
+            </MuiPickersUtilsProvider>
           </div>
           <DialogCreateGame />
         </section>
