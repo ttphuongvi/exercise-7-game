cb9d44e86d37fb088d7f629114e7a19fda860df6
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-02 16:30:37 +0700

    rename classes

--- src/Atomic/molecules/ButtonStyle1/index.js
+++ src/Atomic/molecules/ButtonStyle1/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import AtomButton from "../../atoms/AtomButton";
-import { useNavigate } from "react-router-dom";
 import useStyles from "./styles";
 import clsx from "clsx";
 
 const ButtonStyle1 = (props) => {
   const classes = useStyles();
-  const { customButton, button3 } = classes;
-  const styleButton = clsx(customButton, button3);
+  const { customButton, button } = classes;
+  const styleButton = clsx(customButton, button);
   return (
     <AtomButton onClick={props.onClick} className={styleButton}>
       <span>{props.label}</span>
