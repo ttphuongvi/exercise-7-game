2a3d472482761974552a81b78a8b8f8a3c98e46e
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-09 08:14:21 +0700

    add template LinkStyle textDerectation is none

---
+++ src/Atomic/templates/TemplateTag/LinkStyle.js
@@ -0,0 +1,7 @@
+import { styled } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const LinkStyle = styled(Link)({
+  textDecoration: "none",
+});
+export default LinkStyle;
