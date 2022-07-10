3aa7c8623c14c3e429e386efd2644dbf53239225
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-11 05:50:52 +0700

    edit style container in detaigame

--- src/Atomic/templates/TemplateDetailGame.js
+++ src/Atomic/templates/TemplateDetailGame.js
@@ -3,26 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import TitleCatogery from "../molecules/TitleCatogery";
 import AtomCard from "../atoms/AtomCard";
 import AtomCardContent from "../atoms/AtomCardContent";
-import TabAppBar from "../molecules/TabAppBar";
 import AtomCardAction from "../atoms/AtomCardAction";
 
 const useStyles = makeStyles({
-  containerDetailGame: {
-    width: "50%",
-    background: "#fff",
-    border: "3px solid #25394a",
-    borderRadius: "10px",
-    margin: "10px 0px 30px 10px",
-    // -webkit-box-shadow: '0px 2px 5px 2px rgb(0 0 0 / 50%)',
-    boxShadow: "0px 2px 5px 2px rgb(0 0 0 / 50%)",
-    position: "relative",
-  },
   titleCatogery: {
-    margin: "70px auto 0px auto",
+    margin: "0 auto",
     width: "60%",
-  },
-  card: {
-    // boxShadow: "none",
   },
   action: {
     justifyContent: "center",
@@ -34,7 +20,6 @@ const TemplateDetailGame = (props) => {
 
   return (
     <div>
-      <TabAppBar />
       <TitleCatogery className={classes.titleCatogery} title="Chi tiết game">
         <AtomCard elevation={5} className={classes.card}>
           {props.image}
