7cc1bd45a53889a4917a9096695ec76d93d4bc08
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-11 08:30:46 +0700

    add template page home

--- src/Atomic/pages/Home/index.js
+++ src/Atomic/pages/Home/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import GridNewGame from "../../organisms/GridNewGame";
 import Slider from "../../organisms/Slider";
+import TemplateHome from "../../templates/TemplateHome";
 // import logo from "./img/hahalolo-logo.png";
 const Home = ({ navigateTabListgame }) => {
   return (
-    <div>
-      <Slider />
-      <GridNewGame navigateTabListgame={navigateTabListgame} />
-    </div>
+    <TemplateHome
+      slider={<Slider />}
+      gridNewGame={<GridNewGame navigateTabListgame={navigateTabListgame} />}
+    ></TemplateHome>
   );
 };
 
---
+++ src/Atomic/templates/TemplateHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TemplateHome = (props) => {
+  return (
+    <div>
+      {props.slider}
+      {props.gridNewGame}
+    </div>
+  );
+};
+
+export default TemplateHome;
