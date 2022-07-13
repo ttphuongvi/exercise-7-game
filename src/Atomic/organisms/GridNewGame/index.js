de0830f11b027f8fbde3906cd40b08e2676f34bf
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-13 17:07:25 +0700

    convert GridNewGame to styled

--- src/Atomic/organisms/GridNewGame/index.js
+++ src/Atomic/organisms/GridNewGame/index.js
@@ -4,15 +4,36 @@ import { useNavigate } from "react-router-dom";
 import ButtonStyle2 from "../../molecules/ButtonStyle2";
 import ButtonStyle1 from "../../molecules/ButtonStyle1";
 import AtomGrid from "../../atoms/AtomGrid";
-import useStyle from "./styles";
-import TitleCatogery from "../../molecules/TitleCategory";
 import AtomCard from "../../atoms/AtomCard";
-import ContainerImageNewGame from "../../templates/TemplateTag/ContainerImageNewGame";
-import ImageNewGame from "../../templates/TemplateTag/ImageNewGame";
-import ContainerContentNewGame from "../../templates/TemplateTag/ContainerContentNewGame";
-import DescriptionGame from "../../templates/TemplateTag/DescriptionGame";
-import DivFlexRow from "../../templates/TemplateTag/DivFlexRow";
-import CaptionGame from "../../templates/TemplateTag/CaptionGame";
+import ContainerImageNewGame from "../../molecules/ContainerImageNewGame";
+import ImageNewGame from "../../molecules/ImageNewGame";
+import ContainerContentNewGame from "../../molecules/ContainerContentNewGame";
+import DescriptionGame from "../../molecules/DescriptionGame";
+import DivFlexRow from "../../molecules/DivFlexRow";
+import CaptionGame from "../../molecules/CaptionGame";
+import { createTheme, styled } from "@material-ui/core/styles";
+import AtomContainer from "../../atoms/AtomContainer";
+import AtomRouteLink from "../../atoms/AtomRouteLink";
+
+const theme = createTheme();
+
+const CardContainerStyles = styled(AtomCard)({
+  boxShadow:
+    "0 4px 8px 0 rgb(227 211 211 / 20%), 0 6px 20px 0 rgb(168 163 163 / 19%)",
+  padding: theme.spacing(2),
+  "&:hover": {
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    transform: "scale(1.05)",
+  },
+});
+
+const ContainerGridListNewGame = styled(AtomContainer)({
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+});
 
 const GridNewGame = ({ navigateTabListgame }) => {
   const [dataSource, setDataSource] = useState([]);
@@ -34,41 +55,35 @@ const GridNewGame = ({ navigateTabListgame }) => {
     // console.log(dataSource);
   }, []);
 
-  const classes = useStyle();
-
   return (
-    <TitleCatogery title="Game mới nhất">
-      <div className={classes.root}>
-        <AtomGrid className={classes.gridContainer} container spacing={3}>
-          {dataSource.map((value) => {
-            return (
-              <AtomGrid key={value.id} item xs={12} sm={6} md={6} lg={4}>
-                <AtomCard className={classes.paper} elevation={7}>
-                  <CaptionGame className={classes.caption}>
-                    {value.caption}
-                  </CaptionGame>
-                  <DivFlexRow>
-                    <ContainerImageNewGame>
-                      <ImageNewGame src={value.image} alt=""></ImageNewGame>
-                    </ContainerImageNewGame>
-                    <ContainerContentNewGame>
-                      <DescriptionGame>{value.description}</DescriptionGame>
-                      <ButtonStyle1
-                        label="Xem chi tiết"
-                        onClick={() => {
-                          navigate(`/${value.id}`);
-                        }}
-                      />
-                    </ContainerContentNewGame>
-                  </DivFlexRow>
-                </AtomCard>
-              </AtomGrid>
-            );
-          })}
-        </AtomGrid>
-        <ButtonStyle2 onClick={handleClick} label="Xem thêm"></ButtonStyle2>
-      </div>
-    </TitleCatogery>
+    <ContainerGridListNewGame>
+      <AtomGrid container spacing={3}>
+        {dataSource.map((value) => {
+          return (
+            <AtomGrid key={value.id} item xs={12} sm={6} md={6} lg={4}>
+              <CardContainerStyles>
+                <CaptionGame>{value.caption}</CaptionGame>
+                <DivFlexRow>
+                  <ContainerImageNewGame>
+                    <ImageNewGame src={value.image} alt=""></ImageNewGame>
+                  </ContainerImageNewGame>
+                  <ContainerContentNewGame>
+                    <DescriptionGame>{value.description}</DescriptionGame>
+                    <ButtonStyle1
+                      label="Xem chi tiết"
+                      onClick={() => {
+                        navigate(`/${value.id}`);
+                      }}
+                    />
+                  </ContainerContentNewGame>
+                </DivFlexRow>
+              </CardContainerStyles>
+            </AtomGrid>
+          );
+        })}
+      </AtomGrid>
+      <ButtonStyle2 onClick={handleClick} label="Xem thêm"></ButtonStyle2>
+    </ContainerGridListNewGame>
   );
 };
 
