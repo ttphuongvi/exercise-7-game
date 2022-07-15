e27d6d5fe48037a071a7bb3debaa02df97e7fdd7
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-15 16:34:04 +0700

    create theme default

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  status: {
+    black: "#20232A",
+    white: "#FFFFFF",
+    blue: "#2AC0FF",
+  },
+});
+
+export default theme;
