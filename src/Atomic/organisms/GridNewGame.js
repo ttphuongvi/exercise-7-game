a2ee6b43224a9e27b2dd9a035e650f6c6351cbef
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-21 14:45:05 +0700

    set theme gridnewgame

--- src/Atomic/organisms/GridNewGame.js
+++ src/Atomic/organisms/GridNewGame.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import SquareStripeButton from "../molecules/SquareStripeButton";
 import AtomGrid from "../atoms/AtomGrid";
@@ -9,27 +8,16 @@ import ImageNewGame from "../molecules/ImageNewGame";
 import ContainerContentNewGame from "../molecules/ContainerContentNewGame";
 import DescriptionGame from "../molecules/DescriptionGame";
 import CaptionGame from "../molecules/CaptionGame";
-import { createTheme, styled } from "@mui/material/styles";
-import AtomContainer from "../atoms/AtomContainer";
+import { styled } from "@mui/material/styles";
 import getNewGames from "../../services/games";
 
-const theme = createTheme();
-
-const CardContainerStyles = styled(AtomCard)({
-  // boxShadow: theme.shadows[1],
+const CardStyles = styled(AtomCard)(({ theme }) => ({
+  boxShadow: theme.shadows[2],
   padding: theme.spacing(2),
-  "&:hover": {
-    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px;",
-    transform: "scale(1.05)",
-  },
-  transition: "transform 0.3s ",
-});
-
-const ThemeContainer = styled(CardContainerStyles)(({ theme }) => ({
-  boxShadow: theme.shadows[1],
   "&:hover": {
     boxShadow: theme.shadows[2],
     transform: "scale(1.05)",
+    transition: "all 0.3s ease-in-out",
   },
 }));
 
@@ -38,16 +26,12 @@ const GridNewGame = () => {
 
   let navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/menu");
-  };
-
   return (
     <AtomGrid container spacing={3}>
       {data.map((value) => {
         return (
           <AtomGrid key={value.id} item xs={12} sm={6} md={6} lg={4}>
-            <ThemeContainer>
+            <CardStyles>
               <AtomGrid container spacing={1}>
                 <AtomGrid item xs={12}>
                   <CaptionGame>{value?.caption}</CaptionGame>
@@ -69,7 +53,7 @@ const GridNewGame = () => {
                   </ContainerContentNewGame>
                 </AtomGrid>
               </AtomGrid>
-            </ThemeContainer>
+            </CardStyles>
           </AtomGrid>
         );
       })}
