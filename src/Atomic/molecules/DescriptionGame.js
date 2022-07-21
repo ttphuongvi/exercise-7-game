c59092def80fb5608e59b30b953e73790ee8cc29
ttphuongvi <trthphuongvi2901@gmail.com>
2022-07-21 14:43:05 +0700

    edit Description

--- src/Atomic/molecules/DescriptionGame.js
+++ src/Atomic/molecules/DescriptionGame.js
@@ -4,17 +4,15 @@ import AtomTypography from "../atoms/AtomTypography";
 const DescriptionNewGame = styled(AtomTypography)({
   textAlign: "justify",
   marginTop: "0px",
-  // color: "#666",
   fontSize: "14px",
-  height: "190px",
+  height: "192px",
   width: "100%",
   fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
   marginBottom: "5px",
   overflow: "hidden",
   display: "-webkit-box",
-  /* -webkit-line-clamp: 9; */
   WebkitBoxOrient: "vertical",
-  WebkitLineClamp: 9,
+  WebkitLineClamp: 8,
 });
 
 export default DescriptionNewGame;
