a12ba70106fe0fff32d1eca2126a067888c19994
ttphuongvi <trthphuongvi2901@gmail.com>
2022-08-01 10:26:34 +0700

    edit dataRoute

--- src/routesGame/dataRoutes.js
+++ src/routesGame/dataRoutes.js
@@ -5,50 +5,19 @@ const dataRoutes = [
   {
     name: "Trang chủ",
     path: "/",
-    exact: true,
+
     component: <Home />,
-    hashLink: [
-      {
-        name: "Slider",
-        href: "#slider",
-      },
-      {
-        name: "Game mới nhất",
-        href: "#new-game",
-      },
-    ],
   },
   {
     name: "Danh sách game",
     path: "/menu",
-    // exact: false,
+
     component: <ListGame />,
-    hashLink: [
-      {
-        name: "Tìm kiếm game",
-        href: "#search-game",
-      },
-      {
-        name: "Tạo game",
-        href: "#create-game",
-      },
-      {
-        name: "Danh sách game",
-        href: "#list-game",
-      },
-    ],
   },
   {
     name: "Liên hệ",
     path: "/contact",
-    // exact: "false",
     component: <Contact />,
-    hashLink: [
-      {
-        name: "Form Liên hệ",
-        href: "#form-contact",
-      },
-    ],
   },
 ];
 export default dataRoutes;
