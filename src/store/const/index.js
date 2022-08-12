3d1b21c222d16124f52d71a0a1f46c66280b3d7d
ttphuongvi <trthphuongvi2901@gmail.com>
2022-08-12 10:32:18 +0700

    add boxshadow paper

--- src/store/const/index.js
+++ src/store/const/index.js
@@ -3,3 +3,4 @@ export const ADD_GAME = "ADD_GAME";
 export const INIT_LIST_GAME = [];
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+export const BOXSHAW_PAPER = " rgba(0, 0, 0, 0.14) 0px 3px 8px";
