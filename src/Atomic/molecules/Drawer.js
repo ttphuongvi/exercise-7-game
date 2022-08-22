4abfcccd33502ba262dd72415b85ade460a52481
ttphuongvi <trthphuongvi2901@gmail.com>
2022-08-22 09:38:35 +0700

    add icon for drawer

--- src/Atomic/molecules/Drawer.js
+++ src/Atomic/molecules/Drawer.js
@@ -11,9 +11,11 @@ import AtomDivider from "../atoms/AtomDivider";
 import DialogLogin from "./DialogMaxWidth/DialogLogin";
 import DialogSignUp from "./DialogMaxWidth/DialogSignUp";
 import { useSelector } from "react-redux";
-
 import { alpha } from "@mui/material";
 import AtomRouteLink from "../atoms/AtomRouteLink";
+import AtomListItemIcon from "../atoms/AtomListItemIcon";
+import AtomIconLogin from "../atoms/AtomIconLogin";
+import AtomIconCreate from "../atoms/AtomIconCreate";
 
 const drawerWidth = 240;
 
@@ -53,7 +55,7 @@ const Drawer = (props) => {
               sx={(theme) => ({
                 "&:hover": {
                   backgroundColor: alpha(theme.palette.primary.main, 0.1),
-                  // color: theme.palette.primary.main,
+                  color: theme.palette.primary.main,
                 },
                 "&.active": {
                   color: theme.palette.primary.main,
@@ -61,16 +63,32 @@ const Drawer = (props) => {
               })}
               selected={location.pathname === route.path}
             >
+              <AtomListItemIcon
+                disabled
+                sx={(theme) => ({
+                  color: theme.palette.text.primary,
+                  // "&:hover": {
+                  //   color: theme.palette.primary.main,
+                  // },
+
+                  "&.MuiListItemIcon-root": {
+                    "&.Mui-focusVisible": {
+                      color: theme.palette.primary.main,
+                    },
+                  },
+                })}
+                selected={location.pathname === route.path}
+              >
+                {route.icon}
+              </AtomListItemIcon>
               <AtomListItemText
                 sx={(theme) => ({
                   fontFamily: theme.typography.titleGame.fontFamily,
                   fontSize: theme.typography.body2.fontSize,
                   textTransform: "uppercase",
-                  paddingTop: theme.spacing(1),
-                  paddingBottom: theme.spacing(1),
-                  paddingLeft: theme.spacing(1),
+                  padding: theme.spacing(1, 0, 1, 1),
                   "&.active .MuiTypography-root": {
-                    color: theme.palette.primary.main,
+                    // color: theme.palette.primary.main,
                   },
                 })}
                 disableTypography
@@ -84,16 +102,54 @@ const Drawer = (props) => {
         ) : (
           <>
             <AtomListItemButton
-              sx={{ display: { xs: "flex", md: "flex", lg: "none" } }}
+              sx={(theme) => ({
+                display: { xs: "flex", md: "flex", lg: "none" },
+                "&:hover": {
+                  backgroundColor: alpha(theme.palette.primary.main, 0.1),
+                  color: theme.palette.primary.main,
+                },
+                "&.active": {
+                  color: theme.palette.primary.main,
+                },
+              })}
             >
+              <AtomListItemIcon
+                sx={(theme) => ({
+                  color: theme.palette.text.primary,
+                  "&.active": {
+                    color: theme.palette.primary.main,
+                  },
+                })}
+              >
+                <AtomIconLogin />
+              </AtomListItemIcon>
               <AtomListItemText>
                 {" "}
                 <DialogLogin />
               </AtomListItemText>
             </AtomListItemButton>
             <AtomListItemButton
-              sx={{ display: { xs: "flex", md: "flex", lg: "none" } }}
+              sx={(theme) => ({
+                display: { xs: "flex", md: "flex", lg: "none" },
+                "&:hover": {
+                  backgroundColor: alpha(theme.palette.primary.main, 0.1),
+                  color: theme.palette.primary.main,
+                },
+                "&.active": {
+                  color: theme.palette.primary.main,
+                },
+              })}
             >
+              <AtomListItemIcon
+                sx={(theme) => ({
+                  color: theme.palette.text.primary,
+                  "&.active": {
+                    color: theme.palette.primary.main,
+                  },
+                })}
+              >
+                <AtomIconCreate />
+              </AtomListItemIcon>
               <AtomListItemText>
                 <DialogSignUp />
               </AtomListItemText>
