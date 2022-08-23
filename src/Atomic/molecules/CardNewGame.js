18693e2822c6b31fefdc179f91c70211348fef33
ttphuongvi <trthphuongvi2901@gmail.com>
2022-08-23 16:10:06 +0700

    responsive mobile newgame

--- src/Atomic/molecules/CardNewGame.js
+++ src/Atomic/molecules/CardNewGame.js
@@ -12,43 +12,27 @@ const ItemCard = styled(AtomCard)(({ theme }) => ({
   boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 0px 1px",
   position: "relative",
   transition: "all .2s ease",
-  [theme.breakpoints.up("sm")]: {
-    height: "250px",
-  },
-  [theme.breakpoints.up("xl")]: {
-    height: "300px",
-  },
-  [theme.breakpoints.up("xxl")]: {
-    height: "500px",
-  },
-
-  "&::before": {
-    content: "''",
-    position: "absolute",
-    top: "0",
-    left: "0",
-    width: "100%",
-    height: "100%",
-    background:
-      "rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 80%)",
-    zIndex: 0,
-  },
-
-  "& .detailContent": {
-    maxHeight: 0,
-    opacity: 0,
-    transition: "max-height 1.5s ease, opacity 1s ease",
-  },
-  "&:hover": {
-    boxShadow: "rgba(0, 0, 0, 0.14) 0px 3px 8px",
-    transform: "translateY(-4px)",
-    cursor: "pointer",
-    "& .description": { maxHeight: "250px", opacity: 1 },
-    "& .content": { backgroundColor: "rgba(0, 0, 0, 0.6)", width: "100%" },
-    "& .img": {
-      transform: "scale(1.2)",
-      zIndex: -1,
+  cursor: "pointer",
+  [theme.breakpoints.down("md")]: {
+    "& .MuiCardContent-root:last-child": {
+      paddingBottom: theme.spacing(1),
     },
+    "& .description": {
+      maxHeight: "250px",
+      opacity: 1,
+      transition: "max-height 1.5s ease, opacity 1s ease",
+      height: "50px",
+      overflow: "hidden",
+      display: "-webkit-box",
+      WebkitBoxOrient: "vertical",
+      textOverflow: "ellipsis",
+      WebkitLineClamp: 2,
+    },
+    "& .content": {
+      backgroundColor: "rgba(0, 0, 0, 0.6)",
+      width: "100%",
+    },
+
     "& .action": {
       opacity: 1,
       maxHeight: "100%",
@@ -62,11 +46,67 @@ const ItemCard = styled(AtomCard)(({ theme }) => ({
       color: theme.palette.primary.main,
     },
   },
+  [theme.breakpoints.up("md")]: {
+    height: "250px",
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      background:
+        "rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 80%)",
+      zIndex: 0,
+    },
+    "& .detailContent": {
+      maxHeight: 0,
+      opacity: 0,
+      transition: "max-height 1.5s ease, opacity 1s ease",
+    },
+    "&:hover": {
+      boxShadow: "rgba(0, 0, 0, 0.14) 0px 3px 8px",
+      transform: "translateY(-4px)",
+      cursor: "pointer",
+      "& .description": { maxHeight: "250px", opacity: 1 },
+      "& .content": {
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+        width: "100%",
+      },
+      "& .img": {
+        transform: "scale(1.2)",
+        zIndex: -1,
+        ObjectFit: "cover",
+      },
+      "& .action": {
+        opacity: 1,
+        maxHeight: "100%",
+      },
+      "& .detailContent": {
+        maxHeight: "250px",
+        opacity: 1,
+        transition: "max-height 1.5s ease, opacity 1s ease",
+      },
+      "& .caption": {
+        color: theme.palette.primary.main,
+      },
+    },
+  },
+  [theme.breakpoints.up("xl")]: {
+    height: "300px",
+  },
+  [theme.breakpoints.up("xxl")]: {
+    height: "500px",
+  },
 }));
 
 const CardMediaStyle = styled(AtomCardMedia)(({ theme }) => ({
   transition: "transform 3s ease",
   [theme.breakpoints.up("xs")]: {
+    height: "350px",
+    objectFit: "cover",
+  },
+  [theme.breakpoints.up("md")]: {
     height: "250px",
   },
   [theme.breakpoints.up("xl")]: {
@@ -79,25 +119,27 @@ const CardMediaStyle = styled(AtomCardMedia)(({ theme }) => ({
 }));
 
 const Description = styled(AtomTypography)(({ theme }) => ({
-  maxHeight: 0,
-  opacity: 0,
-  transition: "max-height 1.5s ease, opacity 1s ease",
-  height: "50px",
-  overflow: "hidden",
-  display: "-webkit-box",
-  WebkitBoxOrient: "vertical",
-  textOverflow: "ellipsis",
-  WebkitLineClamp: 2,
+  [theme.breakpoints.up("md")]: {
+    maxHeight: 0,
+    opacity: 0,
+    transition: "max-height 1.5s ease, opacity 1s ease",
+    height: "50px",
+    overflow: "hidden",
+    display: "-webkit-box",
+    WebkitBoxOrient: "vertical",
+    textOverflow: "ellipsis",
+    WebkitLineClamp: 2,
+  },
 }));
 
-const CardContent = styled(AtomCardContent)({
+const CardContent = styled(AtomCardContent)(({ theme }) => ({
   position: "absolute",
   bottom: 0,
   color: "white",
   width: "100%",
   /*     background-color: rgba(0, 0, 0, 0.4); */
   transition: "background-color 2s ease",
-});
+}));
 
 const Caption = styled(AtomTypography)(({ theme }) => ({
   transition: "color 1s ease",
@@ -105,9 +147,11 @@ const Caption = styled(AtomTypography)(({ theme }) => ({
 }));
 
 const CardAction = styled(AtomCardAction)(({ theme }) => ({
-  opacity: 0,
-  maxHeight: 0,
-  transition: "max-height 1.5s ease, opacity 1s ease",
+  [theme.breakpoints.up("md")]: {
+    opacity: 0,
+    maxHeight: 0,
+    transition: "max-height 1.5s ease, opacity 1s ease",
+  },
 }));
 
 const CardNewGame = (props) => {
